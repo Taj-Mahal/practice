5c4283516d8ce2afdd1df58cc5e3e8fac6824334
Taj-Mahal <to1203mo@icloud.com>
2015-01-19 18:12:18 +0900

    Create rissen1.c

---
+++ rissen1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+
+{
+     int array[] = {42,79,13,75,19};
+     int i;
+     
+     
+     for (i = 0;i < 5;i++) {
+          printf("array[%d] = %d\n",i,array[i]);
+	  
+     }
+     
+     return 0;
+}     
